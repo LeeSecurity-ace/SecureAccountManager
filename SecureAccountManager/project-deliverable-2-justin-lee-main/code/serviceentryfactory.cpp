5346c3fce14db9bfed45835c20463788180cec49
LeeSecurity-ace <144623953+LeeSecurity-ace@users.noreply.github.com>
2025-08-16 15:58:24 -0400

    Delete SecureAccountManager/project-deliverable-2-justin-lee-main directory

--- SecureAccountManager/project-deliverable-2-justin-lee-main/code/serviceentryfactory.cpp
+++
@@ -1,6 +0,0 @@
-#include "serviceentryfactory.h"
-#include <QSqlError>
-
-ServiceEntry *ServiceEntryFactory::createEntry(const QString &serviceName, const QString &username, const QString &password) {
-    return new ServiceEntry(serviceName, username, password);
-}
